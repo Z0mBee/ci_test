8060ea6bd6757a39f1635c88f191c6c412476ceb
Andi <daesterer@gmx.net>
2017-03-22 21:00:30 +0100

    Deploy test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel BAM
                 </div>
 
                 <div class="links">
